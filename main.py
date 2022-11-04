b59c13011a2d2bb0fc6fb23c04cd91d6f58d765b
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-05 01:44:41 +0200

    display web page contents

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response = requests.get(url=URL)
+web_contents = response.text
 
-
+soup = BeautifulSoup(web_contents, "html.parser")
+print(soup.prettify())

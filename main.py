8e1469db1bf7e224f8e4d3465173f6c7cda3a4d8
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-05 02:31:05 +0200

    reversed list to start list from 1-100

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 response = requests.get(url=URL)
-web_contents = response.text
+web_contents = response.text    # get the text
 
-soup = BeautifulSoup(web_contents, "html.parser")
-
+soup = BeautifulSoup(web_contents, "html.parser")   # make soup
+# search and find all movie titles
 movie_titles = soup.find_all(name="h3", class_="title")
-movie_list = []
-
+movie_list_copy = []
+# add each title to the list
 for movie_title in movie_titles:
     movie = movie_title.get_text()
-    movie_list.append(movie)
+    movie_list_copy.append(movie)
+movie_list = movie_list_copy[::-1]  # reverse the list
 
+# write to file
 with open("./movies.txt", "w", encoding="utf8") as movie_file:
     for movie in movie_list:
         movie_file.write(f"{movie}\n")
+
